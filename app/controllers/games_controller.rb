564ec3baef20248340fc1496011e0fe431d9908b
yuejn <jennifer8ho@gmail.com>
2016-04-15 01:15:04 +0200

    init games controller skeleton

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,15 @@
+class GamesController < ApplicationController
+
+  def index # GET: games
+  end
+
+  def create # POST: games
+  end
+
+  def show # GET: games/:id
+  end
+
+  def update # PATCH: games/:id
+  end
+
+end
