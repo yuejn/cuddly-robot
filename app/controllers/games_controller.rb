f48b4b6f8fb902339c0b87361acc815099d35c49
yuejn <jennifer8ho@gmail.com>
2016-04-15 01:22:57 +0200

    POST and GET games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,15 @@
 class GamesController < ApplicationController
 
   def index # GET: games
+    @games = Game.all
   end
 
   def create # POST: games
+    @game = Game.create(
+      word: Faker::Hipster.word,
+      guessed_letters: [],
+      status: :busy
+    )
   end
 
   def show # GET: games/:id
